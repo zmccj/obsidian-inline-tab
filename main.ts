2cf4d130c9191499d615d2f9552c56469768b5bd
ZM <zm@etplay.net>
2023-01-13 17:25:16 +0800

    function, setting, hotkeys

--- main.ts
+++ main.ts
@@ -3,83 +3,53 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	useTab: boolean;
+    tabSpaces: number;
+    fillIn: string;
+
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	useTab: false,
+    tabSpaces: 4,
+    fillIn: ' '
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        // console.log('loading plugin inline-tab')
+        await this.loadSettings()
+        // console.log("app:",this.app.vault.config.useTab)
+        this.addCommand({
+            id: "insert-inline-tab",
+            name: "Insert inline tab",
+            hotkeys: [{modifiers:["Mod"], key:"]"}, {modifiers:["Alt"], key:"Tab"}],
+            editorCallback: (editor: Editor) => {
+                const cursor = editor.getCursor()
+                const selection = editor.getSelection();
+                // console.log({cursor, selection})
+                //{ "cursor": { "line": 1, "ch": 7 }, "selection": "lets" }
+                let insertCh = selection.length > 0 ? (cursor.ch-selection.length):cursor.ch
+                let addSome = ''
+                if (this.settings.useTab) {
+                    addSome = '\t'
+                } else {
+                    let tail = this.settings.tabSpaces - (insertCh % this.settings.tabSpaces);
+                    for (let index = 0; index < tail; index++) { addSome += this.settings.fillIn }
+                }
+                editor.replaceRange(addSome, {ch:insertCh, line:cursor.line});
+                editor.setCursor({ch:insertCh + addSome.length, line:cursor.line})
+            },
+          });
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new settingTab(this.app, this));
 	}
 
 	onunload() {
-
+        console.log('unload plugin inline-tab')
 	}
 
 	async loadSettings() {
@@ -91,23 +61,8 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class settingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -120,18 +75,52 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for inline tab plugin.'});
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        const settingTabSpaces = new Setting(containerEl)
+        .setName('Tab spaces : '+this.plugin.settings.tabSpaces)
+        .setDesc('How many spaces to be a inline tab?')
+        .setVisibility(!this.plugin.settings.useTab)
+        .addSlider(val => val
+            .setLimits(1, 10, 1)
+            .setValue(this.plugin.settings.tabSpaces)
+            .onChange(async (value) => {
+                this.plugin.settings.tabSpaces = value;
+                await this.plugin.saveSettings();
+                settingTabSpaces.setName('Tab spaces : '+this.plugin.settings.tabSpaces)
+            }))
+
+        const settingFillIn = new Setting(containerEl)
+        .setName('Fill in charactor:')
+        .setVisibility(!this.plugin.settings.useTab)
+        .setDesc('Leave blank for a space charactor.')
+        .addText(text => text
+            .setValue(this.plugin.settings.fillIn)
+            .onChange(async (value) => {
+                value = value.trim()
+                if (value == ''){ value = ' '}
+                this.plugin.settings.fillIn = value[0];
+                await this.plugin.saveSettings();
+            }));
+
+        new Setting(containerEl)
+        .setName('Use tab instead of spaces?')
+        .setDesc('True for \\t , False for spaces.')
+        .addToggle(val => val
+            .setValue(this.plugin.settings.useTab)
+            .onChange(async (value) => {
+                console.log('useTabNewVal: ' + value);
+                this.plugin.settings.useTab = value;
+                await this.plugin.saveSettings();
+                if (value) {
+                    console.log ('settingTabSpaces', settingTabSpaces)
+                    settingTabSpaces.setVisibility(false)
+                    settingFillIn.setVisibility(false)
+                }else{
+                    settingTabSpaces.setVisibility(true)
+                    settingFillIn.setVisibility(true)
+                }
+            }));
+
+    }
 }
